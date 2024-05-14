d9e77a2be14ad7c3a9f6dee5abff0e2d635d669c
Dima <dima.horbal.dev@gmail.com>
2024-05-13 20:02:26 -0600

    add missed imports for database models

--- portfolio_backend_fastapi/models/__init__.py
+++ portfolio_backend_fastapi/models/__init__.py
@@ -1,3 +1,6 @@
 from .model_base import ModelBase
+from .model_image import ModelImage
 from .model_skill_category import ModelSkillCategory
 from .model_skill import ModelSkill
+from .model_user import ModelUser
+from .model_message import ModelMessage
